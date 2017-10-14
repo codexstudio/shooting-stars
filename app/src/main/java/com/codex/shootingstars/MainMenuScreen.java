a569491457f2a8f6d0e3e6775edbcbaf65880924
unite <zhao.terryy@gmail.com>
2017-10-13 20:25:54 -0400

    Fixed animations

--- app/src/main/java/com/codex/shootingstars/MainMenuScreen.java
+++ app/src/main/java/com/codex/shootingstars/MainMenuScreen.java
@@ -19,7 +19,7 @@ public class MainMenuScreen extends Screen {
         background = g.newPixmap("background.png", Graphics.PixmapFormat.RGB565);
         explosion = g.newPixmap("explosion.png", Graphics.PixmapFormat.ARGB8888);
         g.addAnimation(explosion, 300, 300, 6, 8, 256, 256, true);
-        font = new AndroidFont(48, Typeface.DEFAULT);
+        font = new AndroidFont(48, Typeface.DEFAULT, Color.WHITE);
     }
 
     @Override
@@ -41,7 +41,7 @@ public class MainMenuScreen extends Screen {
         Graphics g = game.getGraphics();
         g.drawPixmap(background, 0, 0);
         g.drawText("Play game", 200, 200, font, Color.WHITE);
-        g.update(deltaTime);
+        g.drawAnimations(deltaTime);
     }
 
     @Override
