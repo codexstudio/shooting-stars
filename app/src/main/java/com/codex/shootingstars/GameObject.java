2da3c01f931c1d1b516a483e097b0846c5d9c9a5
gabep11 <gabrielp_11@hotmail.com>
2017-10-19 19:20:19 -0400

    Character Classes Commit 3

--- app/src/main/java/com/codex/shootingstars/GameObject.java
+++ app/src/main/java/com/codex/shootingstars/GameObject.java
@@ -10,10 +10,12 @@ public abstract class GameObject {
 
     //Members
     protected Vector3 transform;
+    protected Vector3 rotation;
 
     //Constructor
-    protected GameObject() { transform.x = 0; transform.y = 0; transform.z = 0; }
-
-
+    protected GameObject() {
+        transform.x = 0; transform.y = 0; transform.z = 0;
+        rotation.x = 0;  rotation.y = 0;  rotation.z = 0;
+    }
 
 }
--- app/src/main/java/com/codex/shootingstars/PlayerContainer.java
+++ app/src/main/java/com/codex/shootingstars/PlayerContainer.java
@@ -6,9 +6,26 @@ import java.util.List;
 public class PlayerContainer extends GameObject{
 
     //Members
-    private List FriendlyShipList = new ArrayList();
+    private List<FriendlyShip> friendlyShipList = new ArrayList<FriendlyShip>();
+
+    //Setters & Getters
+    public int getShipListSize(){
+        return friendlyShipList.size();
+    }
 
     //Methods
-    
+    public void addShip(FriendlyShip ship) {
+        friendlyShipList.add(ship);
+    }
+
+    public void removeShip(FriendlyShip ship) {
+        friendlyShipList.remove(ship);
+    }
+
+    public void playerContainerUpdateRotations(Vector3 direction) {
+        for (FriendlyShip obj : friendlyShipList) {
+            obj.rotation = direction;
+        }
+    }
 
 }
